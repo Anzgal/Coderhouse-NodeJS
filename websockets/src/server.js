874c31c7dc230513bbd6bdbc64f18a36dfb4554e
Martin Anzures <martinanzures@Martins-MacBook-Air.local>
2023-02-05 01:24:26 -0600

    Minor change

--- websockets/src/server.js
+++ websockets/src/server.js
@@ -24,7 +24,7 @@ app.get("/home", (req, res) => {
   res.render("home");
 });
 
-app.get("/realtime", (req, res) => {
+app.get("/realTimeProducts", (req, res) => {
   res.render("realTimeProducts");
 });
 
